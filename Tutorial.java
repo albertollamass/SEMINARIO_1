1013309d649f538d568f52d67ecfc49b7279e3ed
Alberto Llamas <59082768+albertollamass@users.noreply.github.com>
2021-11-06 00:36:32 +0100

    Update Tutorial.java

--- Tutorial.java
+++ Tutorial.java
@@ -6,23 +6,24 @@ import java.sql.ResultSet;
 public class ConexionJavaOracle {
  
     public static void main( String []arg ){
+        System.out.println("Tutorial para conectarse a la BD\n\n");
         try
         {
             //Se carga el driver JDBC
             DriverManager.registerDriver( new oracle.jdbc.driver.OracleDriver() );
              
             //nombre del servidor
-            String nombre_servidor = "127.0.0.1";
+            String nombre_servidor = "oracle0.ugr.es";
             //numero del puerto
             String numero_puerto = "1521";
-            //SID
-            String sid = "xe";
-            //URL "jdbc:oracle:thin:@nombreServidor:numeroPuerto:SID"
-            String url = "jdbc:oracle:thin:@" + nombre_servidor + ":" + numero_puerto + ":" + sid;
+
+            //URL "jdbc:oracle:thin:@nombreServidor:numeroPuerto/practbd.oracle0.ugr.es"
+            String url = "jdbc:oracle:thin:@" + nombre_servidor + ":" + numero_puerto + "/practbd.oracle0.ugr.es";
  
             //Nombre usuario y password
-            String usuario = "DBAP1";
-            String password = "proyecto1";
+            //tu DNI sin letra al final y al que le has cambiado el primer dígito o letra por una x (minúscula).
+            String usuario = "xDNI";
+            String password = "xDNI";
  
             //Obtiene la conexion
             Connection conexion = DriverManager.getConnection( url, usuario, password );
