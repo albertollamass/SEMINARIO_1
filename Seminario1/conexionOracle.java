dfa6e9e9c9b2cfdec4ea70959f3d23f7376a3734
Jose Maria Ramirez Gonzalez <jm77ramirez@gmail.com>
2021-11-11 15:50:06 +0100

    Clase finalizada

--- Seminario1/conexionOracle.java
+++ Seminario1/conexionOracle.java
@@ -2,9 +2,11 @@
 
 public class conexionOracle {
 
-    void ConexionJavaOracle() {
+    protected Connection conexion;
 
-        System.out.println("Tutorial para conectarse a la BD\n\n");
+    ConexionOracle() {
+
+        System.out.println("Conectándose a la BD\n\n");
         try
         {
             //Se carga el driver JDBC
@@ -23,22 +25,7 @@ public class conexionOracle {
             String password = "xDNI";
     
             //Obtiene la conexion
-            Connection conexion = DriverManager.getConnection( url, usuario, password );
-             
-            //Para realiza una consulta
-            Statement sentencia = conexion.createStatement();
-            ResultSet resultado = sentencia.executeQuery( "SELECT * FROM TIPO_OPERACION" );
-             
-            //Se recorre el resultado obtenido
-            while ( resultado.next() )
-            {
-                //Se imprime el resultado colocando
-                //Para obtener la primer columna se coloca el numero 1 y para la segunda columna 2 el numero 2
-                System.out.println ( resultado.getInt( 1 ) + "\t" + resultado.getString( 2 ) );
-            }
-             
-            //Cerramos la sentencia
-            sentencia.close();
+            conexion = DriverManager.getConnection( url, usuario, password );
         }catch( Exception e ){
             e.printStackTrace();
         }
